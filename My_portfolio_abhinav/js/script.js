39a5fdcb4337361b2e8fd3960d8cf211883deb6d
Abhinav <abhinavdogra649@gmail.com>
2023-07-01 20:05:26 -0700

    Adding resume and animation

--- My_portfolio_abhinav/js/script.js
+++ My_portfolio_abhinav/js/script.js
@@ -1,3 +1,14 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+menuIcon.onclick =()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -15,4 +26,32 @@ window.onscroll = () =>{
             });
         };
     });
+    // let stick navbar
+     let header=document.querySelector('header');
+
+     header.classList.toggle('sticky',window.scrollY >100);
+
+     menuIcon.classList.remove('bx-x');
+     navbar.classList.remove('active');
 }
+
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration:2000,
+    delay:200,
+});
+
+ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+ScrollReveal().reveal('.portfolio-pic,.service-container, .portfolio-box,.contact-section',{origin:'bottom'});
+ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
+ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'})
+
+
+const typed= new Typed('.multext',{
+    strings: ['Backend Developer','Frontend Developer','Fitness Expert','Cloud Engineer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+});
